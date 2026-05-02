0719897943da1788fe65061d2708fa0824aa3416
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2026-05-02 08:37:49 +0300

    Initial Commit

---
+++ OshiUI/OshiUI.swift
@@ -0,0 +1,9 @@
+//
+//  OshiUI.swift
+//  OshiUI
+//
+//  Created by Davud Gunduz on 2.05.2026.
+//
+
+import Foundation
+
---
+++ OshiUITests/OshiUITests.swift
@@ -0,0 +1,19 @@
+//
+//  OshiUITests.swift
+//  OshiUITests
+//
+//  Created by Davud Gunduz on 2.05.2026.
+//
+
+import Testing
+@testable import OshiUI
+
+struct OshiUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
